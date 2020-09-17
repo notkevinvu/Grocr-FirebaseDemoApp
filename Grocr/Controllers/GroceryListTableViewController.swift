f4f5cedc22a161a8d64784ae6ff07106f1c278af
Kevin Vu <kevinvu59@gmail.com>
2020-09-16 23:13:08 -0400

    Adjust editing style table view method to remove grocery item objects properly

--- Grocr/Controllers/GroceryListTableViewController.swift
+++ Grocr/Controllers/GroceryListTableViewController.swift
@@ -67,32 +67,43 @@ class GroceryListTableViewController: UITableViewController {
   
   // MARK: UITableView Delegate methods
   
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return items.count
-  }
-  
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
-    let groceryItem = items[indexPath.row]
-    
-    cell.textLabel?.text = groceryItem.name
-    cell.detailTextLabel?.text = groceryItem.addedByUser
-    
-    toggleCellCheckbox(cell, isCompleted: groceryItem.completed)
-    
-    return cell
-  }
-  
-  override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-    return true
-  }
-  
-  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-    if editingStyle == .delete {
-      items.remove(at: indexPath.row)
-      tableView.reloadData()
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
     }
-  }
+  
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+        let groceryItem = items[indexPath.row]
+
+        cell.textLabel?.text = groceryItem.name
+        cell.detailTextLabel?.text = groceryItem.addedByUser
+
+        toggleCellCheckbox(cell, isCompleted: groceryItem.completed)
+
+        return cell
+    }
+  
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+  
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+
+        if editingStyle == .delete {
+            /*
+             removing an object via the object's reference's removeValue() method
+             will trigger the listener that we defined in addObserverReference()
+             to call its closure
+             
+             and since we set new items and reload the table view via the closure
+             already, we do not need to do so here
+             */
+            let groceryItem = items[indexPath.row]
+            groceryItem.ref?.removeValue()
+        }
+        
+    }
   
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     guard let cell = tableView.cellForRow(at: indexPath) else { return }
