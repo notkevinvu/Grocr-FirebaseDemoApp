ef14f41f3dda928cb8cad124123f9c9323fa5c5b
Kevin Vu <kevinvu59@gmail.com>
2020-09-18 17:37:59 -0400

    Add functionality to sign up and login buttons to create and login users with email authentication. Currently does not get correct user in the GroceryListVC yet though.

--- Grocr/Controllers/LoginViewController.swift
+++ Grocr/Controllers/LoginViewController.swift
@@ -41,40 +41,99 @@ class LoginViewController: UIViewController {
   override var preferredStatusBarStyle: UIStatusBarStyle {
     return .lightContent
   }
-  
-  // MARK: Actions
-  @IBAction func loginDidTouch(_ sender: AnyObject) {
-    performSegue(withIdentifier: loginToList, sender: nil)
-  }
-  
-  @IBAction func signUpDidTouch(_ sender: AnyObject) {
-    let alert = UIAlertController(title: "Register",
-                                  message: "Register",
-                                  preferredStyle: .alert)
     
-    let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+    // MARK: View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // create authentication observer
+        Auth.auth().addStateDidChangeListener { [weak self] (auth, user) in
+            
+            guard let self = self else { return }
+            
+            // test if user exists (successful authentication results in the user
+            // value being populated with the user's info)
+            if user != nil {
+                
+                // on successful authentication, perform segue and clear text fields
+                // we don't need to pass the user to the next controller here
+                self.performSegue(withIdentifier: self.loginToList, sender: nil)
+                self.textFieldLoginEmail.text = nil
+                self.textFieldLoginPassword.text = nil
+            }
+        }
     }
-    
-    let cancelAction = UIAlertAction(title: "Cancel",
-                                     style: .cancel)
-    
-    alert.addTextField { textEmail in
-      textEmail.placeholder = "Enter your email"
-    }
-    
-    alert.addTextField { textPassword in
-      textPassword.isSecureTextEntry = true
-      textPassword.placeholder = "Enter your password"
-    }
-    
-    alert.addAction(saveAction)
-    alert.addAction(cancelAction)
-    
-    present(alert, animated: true, completion: nil)
-  }
   
+    // MARK: Actions
+    
+    @IBAction func loginDidTouch(_ sender: AnyObject) {
+        guard let email = textFieldLoginEmail.text,
+            let password = textFieldLoginPassword.text,
+            email.count > 0,
+            password.count > 0
+            else { return }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            
+            if let error = error, user == nil {
+                
+                let alert = UIAlertController(title: "Sign In Failed", message: error.localizedDescription, preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "OK", style: .default))
+                
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+  
+    @IBAction func signUpDidTouch(_ sender: AnyObject) {
+        let alert = UIAlertController(title: "Register",
+                                      message: "Register",
+                                      preferredStyle: .alert)
+
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            
+            let emailField = alert.textFields![0]
+            let passwordField = alert.textFields![1]
+            
+            guard let emailText = emailField.text,
+                  let passwordText = passwordField.text
+                else { return }
+            
+            // create account with user input text
+            Auth.auth().createUser(withEmail: emailText, password: passwordText) { [weak self] (user, error) in
+                
+                guard let self = self else { return }
+                
+                // user account has been created, authenticate and sign in the new user
+                if error == nil {
+                    Auth.auth().signIn(withEmail: self.textFieldLoginEmail.text!, password: self.textFieldLoginPassword.text!)
+                }
+            }
+        }
+
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel)
+
+        alert.addTextField { textEmail in
+            textEmail.placeholder = "Enter your email"
+        }
+
+        alert.addTextField { textPassword in
+            textPassword.isSecureTextEntry = true
+            textPassword.placeholder = "Enter your password"
+        }
+
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+
+        present(alert, animated: true, completion: nil)
+    }
+
 }
 
+// MARK: Text field delegate methods
 extension LoginViewController: UITextFieldDelegate {
   
   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
